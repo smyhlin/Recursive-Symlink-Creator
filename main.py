cf000b90b092ff24425e5c30609f2f87dfa933cf
s_myhlin <myhlin.c@gmail.com>
2024-03-25 20:39:57 +0200

    Renew Gui && Add Folders & files counting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from tkinter import filedialog, Label
+from tkinter import filedialog, Label, Frame
 from CTkMessagebox import CTkMessagebox
 import os
 import sys
@@ -16,28 +16,39 @@ class RecursiveSymlinkGUI:
 
         self.root = ctk.CTk()
         self.root.title("Recursive Symlink Creator")
-        self.root.geometry("720x240")  # Set window size
-        self.root.resizable(False, False)  # Make non-resizable
-        self.source_label = Label(self.root, text="Source Folder:", bg='#242424', fg='#fff', font='Helvetica 12 bold')
-        self.source_label.pack(fill="x",padx=5)
+        self.root.geometry("720x255")
+        self.root.resizable(False, False)
 
-        self.source_entry = ctk.CTkEntry(self.root)
+        # Source sub-box
+        source_frame = ctk.CTkFrame(self.root)
+        source_frame.pack(fill="x", pady=5, padx=5)
+
+        self.source_label = Label(source_frame, text="Source Folder:", bg='#2b2b2b', fg='#fff', font='Helvetica 12 bold')
+        self.source_label.pack(fill="x", padx=5)
+
+        self.source_entry = ctk.CTkEntry(source_frame)
         self.source_entry.pack(fill="x", padx=5)
-    
-        self.browse_source_button = ctk.CTkButton(self.root, text="Browse", command=self.browse_source)
+
+        self.browse_source_button = ctk.CTkButton(source_frame, text="Browse", command=self.browse_source)
         self.browse_source_button.pack(pady=5)
 
-        self.dest_label = Label(self.root, text="Destination Folder:", bg='#242424', fg='#fff', font='Helvetica 12 bold')
-        self.dest_label.pack(fill="x",padx=5)
+        # Destination sub-box
+        dest_frame = ctk.CTkFrame(self.root)
+        dest_frame.pack(fill="x", pady=5, padx=5)
 
-        self.dest_entry = ctk.CTkEntry(self.root)
+        self.dest_label = Label(dest_frame, text="Destination Folder:", bg='#2b2b2b', fg='#fff', font='Helvetica 12 bold')
+        self.dest_label.pack(fill="x", padx=5)
+
+        self.dest_entry = ctk.CTkEntry(dest_frame)
         self.dest_entry.pack(fill="x", padx=5)
 
-        self.browse_dest_button = ctk.CTkButton(self.root, text="Browse", command=self.browse_dest)
+        self.browse_dest_button = ctk.CTkButton(dest_frame, text="Browse", command=self.browse_dest)
         self.browse_dest_button.pack(pady=5)
 
+        # Create Symlinks button
         self.create_button = ctk.CTkButton(self.root, text="Create Symlinks", command=self.create_symlinks)
-        self.create_button.pack(fill="x",pady=10, padx=5)
+        self.create_button.pack(fill="x", pady=10, padx=5)
+
         self.center_window(self.root)
 
     def center_window(self, window):
@@ -58,11 +69,11 @@ class RecursiveSymlinkGUI:
         path = entry.get()
         if os.path.exists(path):
             self.count_folders_and_files(path)
-            label.config(text=f"{label.cget("text").split(':')[0]}: ({self.folder_count} folders, {self.file_count} files)")
+            label.config(text=f"{label.cget('text').split(':')[0]}: ({self.folder_count} folders, {self.file_count} files)")
             self.folder_count = 0
             self.file_count = 0
         else:
-            label.config(text=f"{label.cget("text").split(':')[0]}: 0")
+            label.config(text=f"{label.cget('text').split(':')[0]}: 0")
 
     def count_folders_and_files(self, path):
         """Counts folders and files within the given path."""
@@ -135,11 +146,9 @@ def isUserAdmin():
 
 
 if __name__ == "__main__":
-    gui = RecursiveSymlinkGUI()
-    gui.run()
-    # if len(sys.argv) < 2:
-    #     if not isUserAdmin():
-    #         run_as_admin()
-    #         sys.exit()
-    #     gui = RecursiveSymlinkGUI()
-    #     gui.run()
+    if len(sys.argv) < 2:
+        if not isUserAdmin():
+            run_as_admin()
+            sys.exit()
+        gui = RecursiveSymlinkGUI()
+        gui.run()
